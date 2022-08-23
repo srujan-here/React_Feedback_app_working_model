303541a565d0b206f8f7ff68894e6d626ec142fc
Srujansujji1717 <puttasrujan20bsc74@iiitkottayam.ac.in>
2022-08-23 21:04:12 +0530

    added required files

---
+++ feedback-app/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+  return (
+   <header>
+   <div className="container">
+
+    <h1>Feedback-app</h1>
+   </div>
+
+   </header>
+  );
+}
+
+export default Header;
