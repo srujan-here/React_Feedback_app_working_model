3dcefeb575bc2c6b3541318e5906725d478377c5
Srujansujji1717 <puttasrujan20bsc74@iiitkottayam.ac.in>
2022-08-23 22:07:52 +0530

    added-header

--- feedback-app/src/components/Header.jsx
+++ feedback-app/src/components/Header.jsx
@@ -1,15 +1,27 @@
 import React from "react";
-
-function Header() {
+import PropTypes from "prop-types";
+function Header({ name, bgcolor, textColor }) {
+  const headerStyles = {
+    backgroundColor: "#000000",
+    color: textColor,
+  };
   return (
-   <header>
-   <div className="container">
-
-    <h1>Feedback-app</h1>
-   </div>
-
-   </header>
+    <header style={headerStyles}>
+      <div className="container">
+        <h1>{name}</h1>
+      </div>
+    </header>
   );
 }
+Header.defaultProps = {
+  name: "Feedback-UI",
+  bgColor: "#000000",
+  textColor: "#ff6a95",
+};
 
+Header.propTypes = {
+  name: PropTypes.string,
+  bgColor: PropTypes.string,
+  textColor: PropTypes.string,
+};
 export default Header;
